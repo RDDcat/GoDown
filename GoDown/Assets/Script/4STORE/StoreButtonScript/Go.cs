ca9ab1ec88ba792fb22922e011d55098b670ee2a
RDDcat <yukmaro@gmail.com>
2022-01-10 22:38:44 +0900

    [ADD] 스토어 폼 추가 & 버튼 스크립트 추가

---
+++ GoDown/Assets/Script/4STORE/StoreButtonScript/Go.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Go : MonoBehaviour
+{
+    public void GoUpgrade()
+    {
+        // 업그레이드 창으로 가기
+        // 업그레이드 창으로 가기 버튼 끄기
+        // 피버 업그레이드 창으로가기 버튼 켜기
+    }
+    public void GoFever()
+    {
+        // 피버 업그레이드 창으로 가기
+        // 피버 업그레이드 창으로 가기 버튼 끄기
+        // 업그레이드 창으로가기 버튼 켜기
+    }
+}
