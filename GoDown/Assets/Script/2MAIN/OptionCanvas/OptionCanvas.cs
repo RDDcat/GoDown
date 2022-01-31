7d9a56ae8bcdbc3981d34f6e4f329068d959e897
RDDcat <yukmaro@gmail.com>
2022-01-31 17:04:39 +0900

    [UPDATE] 튜토리얼 페이지 매커니즘 추가

--- GoDown/Assets/Script/2MAIN/OptionCanvas/OptionCanvas.cs
+++ GoDown/Assets/Script/2MAIN/OptionCanvas/OptionCanvas.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class OptionCanvas : MonoBehaviour
 {
     public Canvas canvas;
+    public GameObject[] tutorial;
+    public GameObject Option;
+
 
     private void Start()
     {
@@ -28,5 +31,40 @@ public class OptionCanvas : MonoBehaviour
         SoundManager.instance.sfxPlay(SoundManager.Sfx.Click);
         // 옵션캔버스 닫기
         canvas.enabled = false;
+    }
+
+    public void GoHelp()
+    {
+        // 도움말 보이기
+        tutorial[0].SetActive(true);
+        tutorial[0].transform.position = transform.position;
+        Option.SetActive(false);
+    }
+
+    public void GoNextHelp()
+    {
+        // 다음 도움말 보이기
+        for(int index = 0; index < tutorial.Length; index++)
+        {
+            if (tutorial[index].activeSelf)
+            {
+                tutorial[index].SetActive(false);
+
+                if(index + 1 == tutorial.Length)
+                {                                    
+                    Option.SetActive(true);
+
+                    return;
+                }
+
+                tutorial[index + 1].SetActive(true);
+
+                tutorial[index + 1].transform.position = transform.position;
+
+                return;
+            }
+        }
+        
+
     }
 }
