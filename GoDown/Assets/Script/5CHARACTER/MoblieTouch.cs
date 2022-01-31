bf4ba4a07794df7e05af966c7bdeae4867b48717
RDDcat <yukmaro@gmail.com>
2022-01-31 19:41:28 +0900

    [UPDATE] 터치 조작법 새로운 버전 만듬 + 버전 조절 가능

--- GoDown/Assets/Script/5CHARACTER/MoblieTouch.cs
+++ GoDown/Assets/Script/5CHARACTER/MoblieTouch.cs
@@ -11,10 +11,12 @@ public class MoblieTouch : MonoBehaviour
     public Text ttt;
     // private
     public bool isTouch;
+    public int touchVersion; // 터치 입력방식 변환
 
     private void Start()
     {
         playerSpeed = AccountManager.instance.accountVO.playerSpeed;
+        touchVersion = PlayerPrefs.GetInt("Touch");
     }
 
     private void Update()
@@ -26,18 +28,22 @@ public class MoblieTouch : MonoBehaviour
     {
         if (isTouch)
         {
+            TouchVer(touchVersion);
+        }
+
+    }
+
+    void TouchVer(int version)
+    {
+        // 버전 1
+        if (version == 1)
+        {
             // 터치 입력시 실행
             if (Input.touchCount > 0)
             {
-
                 Touch touch = Input.GetTouch(0);
                 float offset = playerSpeed * Time.deltaTime;
                 Vector2 mousePos = Camera.main.ScreenToWorldPoint(touch.position);
-
-                // 디버그 텍스트
-                tt.text = touch.position.x.ToString();
-                ttt.text = mousePos.x.ToString();
-
 
                 if (mousePos.x - transform.position.x > 0)
                 {
@@ -47,15 +53,28 @@ public class MoblieTouch : MonoBehaviour
                 {
                     transform.position = new Vector2(transform.position.x - offset, transform.position.y);
                 }
-
-
-
-
             }
         }
+        else // 버전 2
+        {
+            // 터치 입력시 실행
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                float offset = playerSpeed * Time.deltaTime;
+                Vector2 mousePos = Camera.main.ScreenToWorldPoint(touch.position);
 
+                if (mousePos.x - Camera.main.transform.position.x > 0)
+                {
+                    transform.position = new Vector2(transform.position.x + offset, transform.position.y);
+                }
+                else
+                {
+                    transform.position = new Vector2(transform.position.x - offset, transform.position.y);
+                }
+            }
+        }
     }
-
 
     // 외부에서 터치 컨트롤
     public void FreezeTouch()
